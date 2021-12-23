cdc0343839a51787192cfa2a75bb9918f07bd848
Konrad Wasilewski <konradwasilewski@vp.pl>
2021-12-23 01:30:32 +0100

    LikesDB interface made

---
+++ PrzepisyP/Data/ILikesDB.cs
@@ -0,0 +1,11 @@
+﻿namespace PrzepisyP.Data
+{
+    public interface ILikesDB
+    {
+        //Checks whether the recipe is liked by a user or not
+        public bool LikedBy(string recipeId, string userId);
+
+        //Changes whether user likes or dislikes a recipe, sort of a toggle
+        public void LikeToggle(string recipeId, string userId);
+    }
+}
